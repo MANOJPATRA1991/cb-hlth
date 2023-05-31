6148751d77ff1c9048f287938f96a7883dffe559
Manoj Patra <patra.manoj0@gmail.com>
2023-05-31 21:14:16 +0530

    refactor: deterministicPartitionKey implementation

--- dpk.js
+++ dpk.js
@@ -1,28 +1,58 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+const createEncoding = (value, algorithm = "sha3-512", encoding = "hex") => {
+  if (typeof value !== "string") {
+    value = JSON.stringify(value);
+  }
+
+  return crypto.createHash(algorithm).update(value).digest(encoding);
+};
+
+const deterministicPartitionKey = (event) => {
+  let candidate = TRIVIAL_PARTITION_KEY;
 
   if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+    const { partitionKey = "" } = event;
+    candidate = `${partitionKey}` || createEncoding(event);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = createEncoding(candidate);
   }
+
   return candidate;
 };
+
+module.exports = { deterministicPartitionKey };
+
+// const crypto = require("crypto");
+
+// exports.deterministicPartitionKey = (event) => {
+//   const TRIVIAL_PARTITION_KEY = "0";
+//   const MAX_PARTITION_KEY_LENGTH = 256;
+//   let candidate;
+
+//   if (event) {
+//     if (event.partitionKey) {
+//       candidate = event.partitionKey;
+//     } else {
+//       const data = JSON.stringify(event);
+//       candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+//     }
+//   }
+
+//   if (candidate) {
+//     if (typeof candidate !== "string") {
+//       candidate = JSON.stringify(candidate);
+//     }
+//   } else {
+//     candidate = TRIVIAL_PARTITION_KEY;
+//   }
+//   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+//     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+//   }
+//   return candidate;
+// };
